a7dabd639da94151c9885c9796f29ae75033151f
Alvis <Alvis@192.168.1.12>
2017-10-16 17:41:36 -0400

    Renamed the Loan class

---
+++ BankSys/src/Bank/DemandAccount.java
@@ -0,0 +1,17 @@
+package Bank;
+
+public class DemandAccount {
+	double balance;
+	
+	public DemandAccount() {
+		this.balance = 0;
+	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public void depositAmount(double amount) {
+		
+	}
+}
